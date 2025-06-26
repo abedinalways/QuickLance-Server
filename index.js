123033ffab7e115d4b35ad1d0d445de0a05ca3b8
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-06-27 02:04:12 +0600

    deployment complete in vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const verifyToken = require('./verifyToken');
-const allowedOrigins = ['https://quick-lance-server.vercel.app'];
+const allowedOrigins = ['https://quicklance-e9af0.web.app'];
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
